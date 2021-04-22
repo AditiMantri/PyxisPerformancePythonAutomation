a92b7104626c43633017ad7a9574d35af31288b1
Aditi Mantri <aditi.mantri@pyxispm.com>
2021-04-22 18:55:28 +0530

    updated

--- LoginAPI.py
+++ LoginAPI.py
@@ -11,6 +11,10 @@ response_json = loginResponse.json()
 print(response_json)
 token = response_json['data']['key']
 setToken(token)
+print(getToken())
 
 assert loginResponse.status_code == 200
 assert response_json['error'] == False
+
+# for next apis use auth for authentication - tokens
+# api.response = requests.get(url, auth=('token', 'token value'))
